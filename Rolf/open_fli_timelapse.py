5cad11125157f25e4c225cc9e0990f1a3c0228eb
Rolf office <r.harkes@nki.nl>
2018-06-26 17:44:09 +0200

    Can now open timelapse files. However it is quite slow. Would be better if analysis of hyperstacks was also done in the plugin.

--- Rolf/open_fli_timelapse.py
+++ Rolf/open_fli_timelapse.py
@@ -1,11 +1,12 @@
 # @File ReferenceFile
 # @File SampleFile
 
+import time
 #import BioFormats
 from loci.plugins import BF
 from loci.plugins.in import ImporterOptions
 #import Image Calculator for background subtraction
-from ij.plugin import 
+from ij.plugin import ImageCalculator
 #some ij functionality
 from ij import IJ 
 from ij.plugin import HyperStackConverter
@@ -18,19 +19,37 @@ from ij.process.AutoThresholder.Method import Otsu
 # This enables us to stop the script by just calling return.
 def run_script():
 	SamIm = open_fli(SampleFile.getAbsolutePath())
-	SamIm.show()
 	RefIm = open_fli(ReferenceFile.getAbsolutePath())
-	RefIm.show()
 	reftau = RefIm.getStringProperty("PARAMETERS: ACQUISITION SETTINGS - RefLifetime")
 	freq = SamIm.getStringProperty("PARAMETERS: ACQUISITION SETTINGS - Frequency")
-	IJ.run("fdFLIM", "image1=["+SamIm.getTitle()+"] boolphimod=false image2=["+RefIm.getTitle()+"] tau_ref="+reftau+" freq="+freq);
+	phases = SamIm.getStringProperty("FLIMIMAGE: LAYOUT - phases")
+	phases = int(phases)
+	timestamps = SamIm.getStringProperty("FLIMIMAGE: LAYOUT - timestamps")
+	timestamps = int(timestamps)
+	#put phases in z.
+	SamIm = HyperStackConverter.toHyperStack(SamIm,1,phases,timestamps,"xyczt","grayscale")	
+	RefIm.show()
+	SamIm.show()
+	#go frame by frame
+	for frame in range(1,timestamps+1):
+		WindowManager.toFront(WindowManager.getWindow(SamIm.getTitle())) 
+		time.sleep(0.25)
+		IJ.run("Make Substack...", "slices=1-12 frames="+str(frame))
+		substack = IJ.getImage()
+		IJ.run("fdFLIM", "image1=["+substack.getTitle()+"] boolphimod=false image2=["+RefIm.getTitle()+"] tau_ref="+reftau+" freq="+freq);
+		substack.close()
+		if frame == 1:
+			substack = IJ.getImage()
+			substack.setTitle("Lifetime_Result")
+		else:
+			IJ.run("Concatenate...", "  title=Lifetime_Result open image1=Lifetime_Result image2=Lifetimes image3=[-- None --]");
 	SamIm.close()
 	RefIm.close()
 	#Stack to Hyperstack
 	result1 = IJ.getImage()
-	result = HyperStackConverter.toHyperStack(result1,3,1,1,"xyctz","grayscale")
+	result = HyperStackConverter.toHyperStack(result1,3,1,timestamps,"xyctz","grayscale")
 	result1.close()
-	#set to NaN
+	#Otsu Threshold to NaN
 	set_to_nan(result)	
 	#set range and LUT
 	set_range_LUT(result,1,1,4,"Royal")
@@ -39,17 +58,21 @@ def run_script():
 	result.show()
 
 def set_to_nan(img):
-	img.show()
-	img.setC(3)
-	meanintensity = img.getProcessor()
-	meanintensity.setAutoThreshold(Otsu, 1) 
-	IJ.run("Create Selection")
-	IJ.run("Make Inverse")
-	img.setC(2)
-	IJ.run("Set...", "value=NaN")
-	img.setC(1)
-	IJ.run("Set...", "value=NaN")
-	IJ.run("Select None")
+	timestamps = img.getNFrames()
+	print timestamps
+	for frame in range(1,timestamps+1):
+		img.setT(frame)
+		img.setC(3)
+		meanintensity = img.getProcessor()
+		meanintensity.setAutoThreshold(Otsu, 1) 
+		img.show()
+		IJ.run("Create Selection")
+		IJ.run("Make Inverse")
+		img.setC(2)
+		IJ.run("Set...", "value=NaN")
+		img.setC(1)
+		IJ.run("Set...", "value=NaN")
+		IJ.run("Select None")
 	
 def set_range_LUT(img,channel,minval,maxval,lut):
 	img.setC(channel)
